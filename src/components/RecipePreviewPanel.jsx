a8a6c492df1c11890b7c6a6f6a64d50dcf8b0804
trashmans <dgelinas320@gmail.com>
2026-04-27 17:44:08 +0000

    fix: remove iframe sandbox blocking Chrome PDF renderer

--- src/components/RecipePreviewPanel.jsx
+++ src/components/RecipePreviewPanel.jsx
@@ -45,7 +45,6 @@ export default function RecipePreviewPanel({ recipe, onClose }) {
           src={url}
           className="flex-1 w-full rounded border border-border min-h-0"
           title={recipe.name ?? recipe.filename}
-          sandbox="allow-scripts allow-same-origin"
         />
       )}
     </div>
